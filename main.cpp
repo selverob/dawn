9d3a7bedb95b9d768546923efea7c0d472baf609
Róbert Selvek <robert@selvek.me>
2018-05-05 16:42:21 +0200

    Add a basic lexer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <cstring>
+#include <memory>
+#include "lex/Lexeme.h"
+#include "lex/Lexer.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::unique_ptr<char[]> Buf(new char[280]);
+    while (!std::cin.eof()) {
+        std::cin.getline(Buf.get(), 280);
+        size_t Len = std::strlen(Buf.get());
+        Lexer Lex(Buf.get(), Buf.get() + Len);
+        Lexeme Token = Lex.getLexeme();
+        while (Token.K != Lexeme::Kind::ENDOFFILE) {
+            std::cout << Token << ' ';
+            Token = Lex.getLexeme();
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
